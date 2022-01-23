ded688ae514438d73b3b611e6726f13e3879e0dc
kismat02 <kismatmagomedov@yandex.ru>
2022-01-23 22:32:32 +0300

    hw1

--- question_3.sql
+++
@@ -1,3 +0,0 @@
-select count(*)
-from public.yellow_taxi_trips
-where to_char(tpep_pickup_datetime, 'YYYY-mm-dd') = '2021-01-15';
--- question_5.sql
+++
@@ -1,9 +0,0 @@
-select COALESCE("Zone", 'Unknown'), count(*)
-from public.yellow_taxi_trips trips
-join (
-	select "LocationID", "Zone"
-	from public.taxi_zone_lookup
-) zones
-on trips."DOLocationID" = zones."LocationID"
-group by 1
-order by 2 desc;
